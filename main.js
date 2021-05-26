43aab6d93dc0fcea5a113501a4921f83250086e6
IArodriguez99 <73261893+IArodriguez99@users.noreply.github.com>
2021-05-26 16:29:02 -0500

    nav bar dots

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 new fullpage('#fullpage', {
-    autoScrolling:true,
-    anchors:['home','work1','work2','work3','work4']
+    autoScrolling: true,
+    navigation: true,
+    navigationPosition: 'left',
+    anchors:['work1','work2','work3','work4','work5',],
     });
+
+    

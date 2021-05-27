37764fd27e818b12cf14212b1a6a8a2c7bb927f6
IArodriguez99 <73261893+IArodriguez99@users.noreply.github.com>
2021-05-27 17:43:04 -0500

    adding pages

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ new fullpage('#fullpage', {
     autoScrolling: true,
     navigation: true,
     navigationPosition: 'left',
-    anchors:['work1','work2','work3','work4','work5',],
+    anchors:['home','work1','work2','work3','work4','work5',],
     });
 
     

8dd76f5e2fc19be21c6c0a02eb7605e0d764f173
IArodriguez99 <73261893+IArodriguez99@users.noreply.github.com>
2021-06-26 16:09:56 -0500

    Dark mode images

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+const logoImg = document.getElementById('logo');
+const arrow = document.getElementById('arrow');
 const currentTheme = localStorage.getItem('theme');
 
 if (currentTheme) {
@@ -12,17 +14,25 @@ if (currentTheme) {
 function switchTheme(e) {
     if (e.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
-        localStorage.setItem('theme', 'dark');
+        logoImg.setAttribute('src', "./assets/logo-light.svg")
+        arrow.setAttribute('src', "./assets/arrow-light.svg")
+        localStorage.setItem('theme', 'dark', 'src');
+        
 
     }else{
         document.documentElement.setAttribute('data-theme', 'light');
-        localStorage.setItem('theme', 'light');
+        logoImg.setAttribute('src', "./assets/renaiselogo 1.svg");
+        arrow.setAttribute('src', "./assets/arrow.svg")
+        localStorage.setItem('theme', 'light', 'src');
     };    
 };
+
+
 
 toggleSwitch.addEventListener('change', switchTheme, false);
 
 
+//hide nav on scroll
 window.addEventListener('scroll', () => {
     if (window.scrollY > 50) {
         return document.querySelector(".nav").classList.add('hide')
